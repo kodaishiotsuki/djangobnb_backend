297e85ac167103d351b29345faa362ffee2b5e0b
Kodai <0711dr5@gmail.com>
2024-10-09 11:57:55 +0900

    fix:corsエラー対応

--- djangobnb_backend/djangobnb_backend/settings.py
+++ djangobnb_backend/djangobnb_backend/settings.py
@@ -55,7 +55,11 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:8000',
     'http://127.0.0.1:3000',
+    'http://localhost:3000',
+    'http://localhost:8000',
 ]
+
+CORS_ALLOW_CREDENTIALS = True
 
 # RESTフレームワークの設定。JWTを使用する設定が有効です。
 REST_AUTH = {
--- djangobnb_backend/djangobnb_backend/urls.py
+++ djangobnb_backend/djangobnb_backend/urls.py
@@ -1,8 +1,17 @@
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 
+# URLパターンのリストを定義
 urlpatterns = [
+    # 管理画面へのアクセスURLを定義 ('admin/' でアクセス可能)
     path('admin/', admin.site.urls),
-    path('api/properties/',include('property.urls')),
+    
+    # 'api/properties/' にアクセスした際に、propertyアプリのurls.pyに定義されたURLパターンを含める
+    path('api/properties/', include('property.urls')),
 ]
+
+# 開発環境でメディアファイル（画像など）にアクセスできるようにする設定
+# settings.py の MEDIA_URL に対応するURLパスから、MEDIA_ROOT に保存されたファイルにアクセス可能にする
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
